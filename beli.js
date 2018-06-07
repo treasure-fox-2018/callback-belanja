d3eafd6d3013efd3733dc382edc5008dc12d12b1
lockonmaram <maramstratos@gmail.com>
2018-06-07 14:21:40 +0700

    belanja done in index.js

--- beli.js
+++ beli.js
@@ -1,19 +1,3 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item}, uangnya kurang ${obj.harga - uang}`);
-      cb(uang)
-    }
-  }, obj.waktu);
-}
-
-module.exports = beli;
-
 // function beli(uang, obj, cb){
 //   console.log(`Saya pergi membeli ${obj.item}`)
 //   setTimeout(function(){
@@ -22,10 +6,26 @@ module.exports = beli;
 //       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
 //       cb(kembalian)
 //     }else{
-//       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-//       cb(kembalian)
+//       console.log(`uang gk cukup nih buat beli ${obj.item}, uangnya kurang ${obj.harga - uang}`);
+//       cb(uang)
 //     }
 //   }, obj.waktu);
 // }
 //
 // module.exports = beli;
+
+function beli(uang, obj, cb){
+  console.log(`Saya pergi membeli ${obj.item}`)
+  setTimeout(function(){
+    let kembalian = uang - obj.harga
+    if (kembalian > 0) {
+      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+      cb(kembalian)
+    }else{
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+      cb(kembalian)
+    }
+  }, obj.waktu);
+}
+
+module.exports = beli;
